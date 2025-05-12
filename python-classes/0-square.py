3758f399b18d91c9ebaf7477558d7fcab39e5093
allaynnn <aynurallahveri@mail.ru>
2025-05-12 16:30:17 +0000

    task-0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3
-"""
-This module defines a class Square that represents a square.
+"""This module defines a Square class."""
 
-A square has equal sides, and this class uses a private attribute `__size` to store
-the size of one side of the square. The size attribute is private to ensure
-encapsulation and control over future updates or validations.
-"""
 
 class Square:
-    """
-    Represents a square.
+    """Represents a square"""
 
-    The size attribute is private, ensuring encapsulation and protecting 
-    the square's internal state from direct access or modification. 
-    Future enhancements or validations can be added for the size.
-    """
     def __init__(self, size):
-        """
-        Initialize a new Square instance with a given size.
-
-        Args:
-            size (int): The size of one side of the square (no validation).
-        """
+        """Initializes the square with a private size"""
         self.__size = size
+
